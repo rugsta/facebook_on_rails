eb4e73ad31fe2852e4ab2ff1c8f37fb18a73eba6
rugsta <adefconalert@gmail.com>
2014-01-24 12:44:35 +1100

    DEVISE. pre db migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FacebookOnRails::Application.routes.draw do
+  devise_for :users
   resources :posts
 
   get "pages/home"
@@ -6,7 +7,7 @@ FacebookOnRails::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'pages#home'
+   root "pages#home"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
