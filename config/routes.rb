6a621bcc8348da5fb30288b7312a7831bcacb379
rugsta <adefconalert@gmail.com>
2014-04-09 18:47:39 +1000

    facebook integration with routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 FacebookOnRails::Application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: 'omniauth_callbacks'}
   resources :posts
 
   get "pages/home"
